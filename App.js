bca80be04554e757f56a248385aace5b776e9f73
KoushikMajumder <114131081+INC02258@users.noreply.github.com>
2026-03-20 21:49:41 +0530

    episode3

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
   import React from "react";
   import ReactDOM from "react-dom/client";
-  const newEle=React.createElement("div",{},[React.createElement("h1",{id:'heading',key:'1'},"hello"),React.createElement("h2",{key:'2'},"world")]);
-    
-  
+  // const newEle=React.createElement("div",{},[React.createElement("h1",{id:'heading',key:'1'},"hello"),React.createElement("h2",{key:'2'},"world")]);
+    const JSXheading=(<h1>Namaste React </h1>);
+    const Title=()=>{
+      return (<h1>Title</h1>)
+    }
+    const HeadingComponent=()=>{
+    return (<div id='container'>
+<Title/>
+<h2>sub header</h2>
+    </div>)
+    }
   
   const root=ReactDOM.createRoot(document.getElementById('root'));
-    root.render(newEle);
+    // root.render(JSXheading);
+    root.render(<HeadingComponent/>);

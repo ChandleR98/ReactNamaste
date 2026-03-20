bcc837880e907284fa6359cfda91adce13d57c66
KoushikMajumder <114131081+INC02258@users.noreply.github.com>
2026-03-20 17:00:14 +0530

    episode2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-  const newEle=React.createElement("div",{},[React.createElement("h1",{id:'heading'},"hello"),React.createElement("h2",{},"world")]);
+  import React from "react";
+  import ReactDOM from "react-dom/client";
+  const newEle=React.createElement("div",{},[React.createElement("h1",{id:'heading',key:'1'},"hello"),React.createElement("h2",{key:'2'},"world")]);
     
   
   

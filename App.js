b0565763a1c6f28c10dff6c0d496da381a5eaa5f
KoushikMajumder <114131081+INC02258@users.noreply.github.com>
2026-03-20 14:42:37 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,6 @@
+  const newEle=React.createElement("div",{},[React.createElement("h1",{id:'heading'},"hello"),React.createElement("h2",{},"world")]);
+    
+  
+  
+  const root=ReactDOM.createRoot(document.getElementById('root'));
+    root.render(newEle);
